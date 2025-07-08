85c977f213e2f450c89fc7e18443dbc729ce1879
shailichou <shailichoudhary2003@gmail.com>
2025-07-08 20:59:35 +0530

    test_clip

---
+++ test_clip.py
@@ -0,0 +1,6 @@
+from transformers import CLIPProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+print("CLIP is working ✅")
